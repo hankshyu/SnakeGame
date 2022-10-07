3a36e8191bfd1fba443bae9e3cfcc5a65cd5714d
NCTUMIT <54198226+NCTUMIT@users.noreply.github.com>
2022-10-07 23:51:30 +0800

    Add files via upload

---
+++ src/design_sources/S0_P3.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Yu-Wei Chen 
+// 
+// Create Date: 2021/12/14 23:59:49
+// Design Name: 
+// Module Name: S0_P3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module S0_P3
+#(parameter DATA_WIDTH = 8, ADDR_WIDTH = 16, RAM_SIZE = 65536)
+ (input clk, input we, input en,
+  input  [ADDR_WIDTH-1 : 0] addr,
+  input  [DATA_WIDTH-1 : 0] data_i,
+  output reg [DATA_WIDTH-1 : 0] data_o);
+
+// Declareation of the memory cells
+(* ram_style = "block" *) reg [DATA_WIDTH-1 : 0] RAM [RAM_SIZE - 1:0];
+
+integer idx;
+
+// ------------------------------------
+// SRAM cell initialization
+// ------------------------------------
+// Initialize the sram cells with the values defined in "image.dat."
+initial begin
+    $readmemh("s0p3.mem", RAM);
+end
+
+// ------------------------------------
+// SRAM read operation
+// ------------------------------------
+always@(posedge clk)
+begin
+  if (en & we)
+    data_o <= data_i;
+  else
+    data_o <= RAM[addr];
+end
+
+// ------------------------------------
+// SRAM write operation
+// ------------------------------------
+always@(posedge clk)
+begin
+  if (en & we)
+    RAM[addr] <= data_i;
+end
+
+endmodule
